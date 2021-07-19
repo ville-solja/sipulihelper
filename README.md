a48de279fe31b81096574b96168c59fa508bfaa3
ville-solja <ville.solja@gmail.com>
2021-07-19 18:44:27 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# SipuliHelper
+
+## General
+This simple bot checks if someone has joined the voice channel at position 1. and this can be used to send further commands*
+
+*Commands not included
+
+## Installation
+### Bot setup
+* Start of by creating a new bot/application (https://discord.com/developers/applications)
+* Select OAuth2 scope "bot" and the following rights:
+    * View channels
+    * Connect
+    * Use Voice Activity
+* Add the bot the server of your choice
+* Great! Now you can fetch the TOKEN needed to run the python script (Available in the bot -tab)
+
+### Discord bot token
+In order to provide access for the bot you need to provide it with the appropriate access token 
+
+Example: "TOKEN=token with length of 59 characters"
---
+++ sipulihelper.py
@@ -0,0 +1,37 @@
+import discord
+from datetime import datetime
+import os
+
+TOKEN = os.environ['TOKEN']    
+#TOKEN = 'NDQ1MTg5MTE0MDMyMDI5NzE2.Wvgkqw.jePYCAWWKdzC0U89C8gm-r5kEZw'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('{} bot started'.format(
+        datetime.now().strftime("%d.%m.%Y, %H:%M:%S")))
+    print('Client Name: {}'.format(
+        client.user.name))
+    print('Client ID: {}'.format(
+        client.user.id))
+    for guild in client.guilds:
+        print('Guild: {}'.format(
+            guild.name))
+        for voice_channel in guild.voice_channels:
+            if voice_channel.position == 1:
+                print('Voice Channel: {}'.format(
+                    voice_channel))
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    for voice_channel in member.guild.voice_channels:
+        if voice_channel.position == 1:
+            if len(voice_channel.members) > 0:
+                print("Users in voice channel: {}".format(len(voice_channel.members)))
+                #Start
+            else:
+                print("Users in voice channel: {}".format(len(voice_channel.members)))
+                #Pause server
+
+client.run(TOKEN)
